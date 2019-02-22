cd2c775130699d8e7fe3234bd98eb25ea805de6c
rxliuli <rxliuli@gmail.com>
2019-02-22 15:38:54 +0800

    初始化 SpringBoot 项目

---
+++ src/main/java/com/rxliuli/example/springbootmybatisplusmongo/SpringBootMybatisPlusMongoApplication.java
@@ -0,0 +1,13 @@
+package com.rxliuli.example.springbootmybatisplusmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisPlusMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisPlusMongoApplication.class, args);
+    }
+
+}
